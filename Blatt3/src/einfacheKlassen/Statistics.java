7b6c80e7b84f888e541dd58fe61b5bee54bde16c
lejla.gusic <lejla.gusic@rwth-aachen.de>
2018-11-12 13:00:40 +0100

    update

--- Blatt3/src/einfacheKlassen/Statistics.java
+++ Blatt3/src/einfacheKlassen/Statistics.java
@@ -4,17 +4,14 @@ public class Statistics {
 	
 	public static void main(String[] args) {
 		Statistics statistics = new Statistics();
-		statistics.addValue(2);
-		statistics.addValue(105);
-		statistics.addValue(-366);
-		statistics.addValue(44);
-		statistics.addValue(11);
+		statistics.addValue(-1);
+		statistics.addValue(1);
 		SimpleIO.output("Minimum: " + statistics.getMinimum());
 		SimpleIO.output("Maximum: " + statistics.getMaximum());
 		SimpleIO.output("Durchschnitt: " + statistics.getAverage());
 	}
 
-	int [] werte = new int [100];
+	int[] werte = new int [100];
 	int laenge = 0;
 	
 	public void addValue(int value) {
@@ -28,13 +25,14 @@ public class Statistics {
 	}
 
 	public int getMinimum() {
-		int result = werte[0];
 		if (laenge == 0) {
 			SimpleIO.output("Fehler : keine Daten");
 			return 0;
 		}
-		for (int i = 0; i < laenge; i++) {
-			if (result > werte[i]) {
+		
+		int result = werte[0];
+		for (int i = 1; i < laenge; i++) {
+			if (werte[i] < result) {
 				result = werte[i];
 			}
 		}
@@ -42,13 +40,14 @@ public class Statistics {
 	}
 
 	public int getMaximum() {
-		int result = werte[0];
 		if (laenge == 0) {
 			SimpleIO.output("Fehler : keine Daten");
 			return 0;
 		}
-		for (int i = 0; i < laenge; i++) {
-			if (result < werte[i]) {
+
+		int result = werte[0];
+		for (int i = 1; i < laenge; i++) {
+			if (werte[i] > result) {
 				result = werte[i];
 			}
 		}
@@ -60,6 +59,7 @@ public class Statistics {
 			SimpleIO.output("Fehler : keine Daten");
 			return 0;
 		}
+		
 		double result = 0;
 		for(int i = 0; i < laenge; i++) {
 			result += werte[i];
